ce81e92fe7ee190fc4ec03d085d93a0059c847b9
GionaldoCandr <gionaldocandrawansah@gmail.com>
2026-03-10 16:55:01 +0700

    ini bang postest nanti saya rapikan lagi

--- src/postest.java
+++ src/postest.java
@@ -1,46 +1,61 @@
 import java.util.Scanner;
 
 public class postest {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-    
-        System.out.println("Simulasi Mentu ATM");
 
-    
-        int saldo= 500000;
+        System.out.println("Simulasi Menu ATM");
 
+        int saldo = 500000;
+        int pilihan;
 
         do {
-            System.out.println("1. Cek saldo| 2. Setor Tunai| 3. Tarik Tunai| 4. Keluar");
+            System.out.println("\n1. Cek Saldo | 2. Setor Tunai | 3. Tarik Tunai | 4. Keluar");
             System.out.print("Pilihan: ");
-            int pilihan = input.nextInt();
-            
+            pilihan = input.nextInt();
+
             switch (pilihan) {
+
                 case 1:
                     System.out.println("Saldo Anda: Rp." + saldo);
                     break;
+
                 case 2:
                     System.out.print("Masukkan jumlah setoran: ");
                     int setoran = input.nextInt();
                     saldo += setoran;
-                    System.out.println("Setoran berhasil. Saldo Anda sekarang: Rp. " + saldo );
+                    System.out.println("Setoran berhasil. Saldo Anda sekarang: Rp." + saldo);
                     break;
+
                 case 3:
-                    System.out.print("Masukkan jumlah penarikan: ");
-                    int penarikan = input.nextInt();
-                    if (penarikan <= 50000) {
-                        System.out.println("Penarikan berhasil. Saldo Anda sekarang: Rp. " + (100000 - penarikan));
-                    } else {
-                        System.out.println("Penarikan tidak dapat dilakukan. Saldo Anda tidak mencukupi.");
+                    if (saldo <= 50000){
+                        System.out.println("Saldo tidak mencukupi maaf yeee");
+                        break;
+                    }else {
+
+                        System.out.print("Masukkan jumlah penarikan: ");
+                        int penarikan = input.nextInt();
+                        
+                        if (penarikan <= saldo) {
+                            saldo -= penarikan;
+                            System.out.println("Penarikan berhasil. Saldo Anda sekarang: Rp." + saldo);
+                        } else {
+                            System.out.println("Saldo tidak mencukupi.");
+                        }
+                        break;
                     }
+
+                case 4:
+                    System.out.println("Terima kasih telah menggunakan layanan ATM kami.");
                     break;
-        
 
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
 
+        } while (pilihan != 4);
 
-
-        } while ( pilihan  == 4);
-
-}
-    
+        input.close();
+    }
 }
