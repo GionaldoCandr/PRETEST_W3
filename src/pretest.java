ca63d151908602327fa666460a580eb9236e2733
GionaldoCandr <gionaldocandrawansah@gmail.com>
2026-03-10 14:18:32 +0700

    pretest

---
+++ src/pretest.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class pretest {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print( "===== SISTEM KELAYAKAN DONOR DARAH ====");
+        System.out.print("\nUsia :");
+            int usia = input.nextInt();
+        if ( usia < 18 ){
+            System.out.print("usia tidak memenuhi kriteria(silahkan input ulang )");
+            return;
+        }else {
+            System.out.print("Usia memenuhi kriteria ( lanjutkan input dibawah) ");
+        }
+        System.out.print("\nBerat Badan:");
+            float berat = input.nextFloat();
+             if ( berat < 64 ){
+            System.out.print("berat tidak memenuhi kriteria(silahkan input ulang )");
+            return;
+        }else {
+            System.out.print("berat memenuhi kriteria ( lanjutkan input dibawah) ");
+        }
+        System.out.print("\nKadar Hemogoblin:");
+            float kadar = input.nextFloat();
+        if ( kadar < 12.5 ){
+            System.out.print("kadar tidak memenuhi kriteria(silahkan input ulang )");
+            return;
+        }else {
+            System.out.print("kadar memenuhi kriteria ( lanjutkan input dibawah) ");
+        }
+
+        System.out.print("\n========HASIL KELAYAKAN========");
+        System.out.print("\nSemua Memenuhi Kriteria");
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   }
+}
